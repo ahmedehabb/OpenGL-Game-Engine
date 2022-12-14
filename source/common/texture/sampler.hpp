a3c1dd6a4ac6bf40a8a81964e0d0d680f795e318
mohamedafifi13 <61080870+mohamedafifi13@users.noreply.github.com>
2022-12-14 21:37:35 +0200

    done with no testing again :(

--- source/common/texture/sampler.hpp
+++ source/common/texture/sampler.hpp
@@ -14,33 +14,61 @@ namespace our {
         // This constructor creates an OpenGL sampler and saves its object name in the member variable "name" 
         Sampler() {
             //TODO: (Req 6) Complete this function
+            // this function will create a sampler and save its name in the name variable
+            //first parameter is the number of samplers that i want to create
+            // second parameter is the pointer to the variable that will hold the name of the sampler
+            glGenSamplers(1, &name);
         };
 
         // This deconstructor deletes the underlying OpenGL sampler
         ~Sampler() { 
             //TODO: (Req 6) Complete this function
+            // this function will delete the sampler
+            // first parameter is the number of samplers that i want to delete
+            // second parameter is the pointer to the variable that will hold the name of the sampler
+            glDeleteSamplers(1, &name);
          }
 
         // This method binds this sampler to the given texture unit
         void bind(GLuint textureUnit) const {
             //TODO: (Req 6) Complete this function
+            // this function will bind the sampler to the texture unit
+            // first parameter is the texture unit that i want to bind the sampler to
+            // second parameter is the name of the sampler that i want to bind
+            glBindSampler(textureUnit, name);
         }
 
         // This static method ensures that no sampler is bound to the given texture unit
         static void unbind(GLuint textureUnit){
             //TODO: (Req 6) Complete this function
+            // this function will unbind the sampler from the texture unit
+            // first parameter is the texture unit that i want to unbind the sampler from
+            
+            glBindSampler(textureUnit, 0);
         }
 
         // This function sets a sampler paramter where the value is of type "GLint"
         // This can be used to set the filtering and wrapping parameters
         void set(GLenum parameter, GLint value) const {
             //TODO: (Req 6) Complete this function
+            // this function will set the sampler parameter
+            // first parameter is the name of the sampler that i want to set the parameter for
+            // second parameter is the parameter that i want to set
+            // third parameter is the value of the parameter that i want to set
+            glSamplerParameteri(name, parameter, value);
+            //parameter is filter or wrapping
+            //value could be repeeat or clamp to edge or constant
         }
 
         // This function sets a sampler paramter where the value is of type "GLfloat"
         // This can be used to set the "GL_TEXTURE_MAX_ANISOTROPY_EXT" parameter
         void set(GLenum parameter, GLfloat value) const {
             //TODO: (Req 6) Complete this function
+            // this function will set the sampler parameter
+            // first parameter is the name of the sampler that i want to set the parameter for
+            // second parameter is the parameter that i want to set
+            // third parameter is the value of the parameter that max anistropy
+            glSamplerParameterf(name, parameter, value);
         }
 
         // This function sets a sampler paramter where the value is of type "GLfloat[4]"
--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -11,12 +11,21 @@ namespace our {
     public:
         // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
         Texture2D() {
-            //TODO: (Req 5) Complete this function
+                  //TODO: (Req 5) Complete this function
+            // this function will create a texture and save its name in the name variable
+            //first parameter is the number of textures that i want to create
+            // second parameter is the pointer to the variable that will hold the name of the texture
+            glGenTextures(1, &name);
+
         };
 
         // This deconstructor deletes the underlying OpenGL texture
         ~Texture2D() { 
-            //TODO: (Req 5) Complete this function
+             //TODO: (Req 5) Complete this function
+            // this function will delete the texture
+            // first parameter is the number of textures that i want to delete
+            // second parameter is the pointer to the variable that will hold the name of the texture
+            glDeleteTextures(1, &name);
         }
 
         // Get the internal OpenGL name of the texture which is useful for use with framebuffers
@@ -26,12 +35,20 @@ namespace our {
 
         // This method binds this texture to GL_TEXTURE_2D
         void bind() const {
-            //TODO: (Req 5) Complete this function
+             //TODO: (Req 5) Complete this function
+            // this function will bind the texture to the GL_TEXTURE_2D
+            // first parameter is the target that i want to bind the texture to which is GL_TEXTURE_2D creating a 2D texture
+            // second parameter is the name of the texture that i want to bind
+
+            glBindTexture(GL_TEXTURE_2D, name);
         }
 
         // This static method ensures that no texture is bound to GL_TEXTURE_2D
         static void unbind(){
-            //TODO: (Req 5) Complete this function
+             //TODO: (Req 5) Complete this function
+            // this function will unbind the texture from the GL_TEXTURE_2D
+            // first parameter is the target that i want to unbind the texture from which is GL_TEXTURE_2D creating a 2D texture
+            glBindTexture(GL_TEXTURE_2D, 0);
         }
 
         Texture2D(const Texture2D&) = delete;
