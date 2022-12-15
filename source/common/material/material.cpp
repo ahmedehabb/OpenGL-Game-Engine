46618ef895b1ce2bedcd5b5feb6d395e1b22c2a6
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-12-15 15:19:57 +0200

    materials shaders,functions setup/ error in run :(

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -8,6 +8,10 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
+        // setup the pipeline state 
+        this->pipelineState.setup();
+        // set the shader to be used
+        this->shader->use();
     }
 
     // This function read the material data from a json object
@@ -25,6 +29,13 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        
+        //call the setup of its parent which is Material
+        Material::setup();
+        
+        // set the "tint" uniform to the value in the member variable tint 
+        // uniform vec4 tint;
+        this->shader->set("tint", this->tint);
     }
 
     // This function read the material data from a json object
@@ -39,6 +50,24 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        
+        //call the setup of its parent which is TintedMaterial
+        TintedMaterial::setup();
+        
+        // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
+        this->shader->set("alphaThreshold", this->alphaThreshold);
+
+        // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
+        // we can use the first one since the shader has only this one transform and we dont need to add to GL_TEXTURE0
+        // i also made textureUnitIndex to keep it dynamic if we needed to change the texture unit after some time
+        int textureUnitIndex = 0;
+        glActiveTexture(GL_TEXTURE0 + textureUnitIndex); 
+        // binding the texture to the active unit of the texture units
+        this->texture->bind();
+        // bind the sampler to the same texture unit index where the texture is binderd
+        this->sampler->bind(textureUnitIndex);
+        // send the unit number to the uniform variable "tex" 
+        this->shader->set("tex", textureUnitIndex);
     }
 
     // This function read the material data from a json object
