1553170fc3eee5d7f64d51c857cb9445302ea9fd
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-12-15 13:13:11 +0200

    made all world funtions

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -26,7 +26,13 @@ namespace our {
         Entity* add() {
             //TODO: (Req 8) Create a new entity, set its world member variable to this,
             // and don't forget to insert it in the suitable container.
-            return nullptr;
+            // Create a new entity
+            Entity* entity = new Entity();
+            // set its world member variable to this
+            entity->world = this;
+            // insert it in the suitable container
+            entities.insert(entity);
+            return entity;
         }
 
         // This returns and immutable reference to the set of all entites in the world.
@@ -38,17 +44,30 @@ namespace our {
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
             //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            if(entities.find(entity) != entities.end()){
+                markedForRemoval.insert(entity);
+            }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
             //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+            for (Entity* entity : markedForRemoval){
+                entities.erase(entity);
+            }
+            // at the end marked for removal contains all entities that are erased so we must clear it
+            markedForRemoval.clear();
         }
 
         //This deletes all entities in the world
         void clear(){
             //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+            if(entities.size() != 0){
+                entities.erase(entities.begin(), entities.end());
+            }
+            // at the end marked for removal could contain entities that are erased so we must clear it
+            markedForRemoval.clear();
         }
 
         //Since the world owns all of its entities, they should be deleted alongside it.
