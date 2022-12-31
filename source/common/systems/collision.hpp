cbc16eaf386e5e0bff222c669e229a75046dedd3
mohamedafifi13 <61080870+mohamedafifi13@users.noreply.github.com>
2023-01-01 00:33:49 +0200

    pushed all

--- source/common/systems/collision.hpp
+++ source/common/systems/collision.hpp
@@ -1,77 +1,77 @@
-// #pragma once
-// #define COIN_SCORE 5
+#pragma once
+#define COIN_SCORE 5
 
-// #include "../ecs/world.hpp"
-// #include "../components/collision.hpp"
-// #include <systems/game-manager.hpp>
+#include "../ecs/world.hpp"
+#include "../components/collision.hpp"
+#include <systems/game-manager.hpp>
 
-// #include <glm/glm.hpp>
-// #include <glm/gtc/constants.hpp>
-// #include <glm/trigonometric.hpp>
-// #include <glm/gtx/fast_trigonometry.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/gtx/fast_trigonometry.hpp>
 
-// #include <iostream>
-// namespace our
-// {
+#include <iostream>
+namespace our
+{
 
-//     // The movement system is responsible for moving every entity which contains a MovementComponent.
-//     // This system is added as a simple example for how use the ECS framework to implement logic.
-//     // For more information, see "common/components/movement.hpp"
-//     class CollisionSystem
-//     {
-//     public:
-//         // This should be called every frame to update all entities containing a MovementComponent.
-//         void update(World *world)
-//         {
+    // The movement system is responsible for moving every entity which contains a MovementComponent.
+    // This system is added as a simple example for how use the ECS framework to implement logic.
+    // For more information, see "common/components/movement.hpp"
+    class CollisionSystem
+    {
+    public:
+        // This should be called every frame to update all entities containing a MovementComponent.
+        void update(World *world)
+        {
 
-//             glm::vec3 carPosition = glm::vec3(0, 0, 0);
-//             float carRadius = 0;
+            glm::vec3 carPosition = glm::vec3(0, 0, 0);
+            float carRadius = 0;
 
-//             Entity *carMesh = nullptr;
+            Entity *carMesh = nullptr;
 
-//             for (auto entity : world->getEntities())
-//             {
-//                 if (entity->name == "car")
-//                 {
-//                     carMesh = entity;
-//                     break;
-//                 }
-//             }
+            for (auto entity : world->getEntities())
+            {
+                if (entity->name == "car")
+                {
+                    carMesh = entity;
+                    break;
+                }
+            }
 
-//             if (!carMesh)
-//                 return;
+            if (!carMesh)
+                return;
 
-//             // get the current player entity and get its position and radius
+            // get the current player entity and get its position and radius
 
-//             CollisionComponent *collision = carMesh->getComponent<CollisionComponent>();
+            CollisionComponent *collision = carMesh->getComponent<CollisionComponent>();
 
-//             carPosition = collision->center + glm::vec3(carMesh->getLocalToWorldMatrix() * glm::vec4(0, 0, 0, 1));
-//             carRadius = collision->radius * glm::length(carMesh->localTransform.scale) / 1.732f;
+            carPosition = collision->center + glm::vec3(carMesh->getLocalToWorldMatrix() * glm::vec4(0, 0, 0, 1));
+            carRadius = collision->radius * glm::length(carMesh->localTransform.scale) / 1.732f;
 
-//             for (auto entity : world->getEntities())
-//             {
+            for (auto entity : world->getEntities())
+            {
 
-//                 // get all entities that have collision component
-//                 CollisionComponent *collision = entity->getComponent<CollisionComponent>();
-//                 if (collision && entity->name != "car")
-//                 {
+                // get all entities that have collision component
+                CollisionComponent *collision = entity->getComponent<CollisionComponent>();
+                if (collision && entity->name != "car")
+                {
 
-//                     // get the new radius and position of the entity
-//                     glm::vec3 newPosition = collision->center + glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(0, 0, 0, 1));
-//                     float newRadius = collision->radius * glm::length(entity->localTransform.scale) / 1.732f;
-//                     // compare with player position to check if it collides or not
-//                     if (glm::length(newPosition - carPosition) < carRadius + newRadius)
-//                     {
-//                         // collision occurred
-//                         if (entity->tag == "finish")
-//                         {
-//                             our::GameMananger::gameOver = true;
-//                         }
+                    // get the new radius and position of the entity
+                    glm::vec3 newPosition = collision->center + glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(0, 0, 0, 1));
+                    float newRadius = collision->radius * glm::length(entity->localTransform.scale) / 1.732f;
+                    // compare with player position to check if it collides or not
+                    if (glm::length(newPosition - carPosition) < carRadius + newRadius)
+                    {
+                        // collision occurred
+                        if (entity->tag == "finish")
+                        {
+                            our::GameMananger::gameOver = true;
+                        }
                         
-//                     }
-//                 }
-//             }
-//         }
-//     };
+                    }
+                }
+            }
+        }
+    };
 
-// }
+}
--- source/common/systems/game-manager.hpp
+++ source/common/systems/game-manager.hpp
@@ -1,81 +1,81 @@
-// #pragma once
+#pragma once
 
-// #include "../ecs/world.hpp"
-// #include "../application.hpp"
+#include "../ecs/world.hpp"
+#include "../application.hpp"
 
-// #include <glm/glm.hpp>
-// #include <glm/gtc/constants.hpp>
-// #include <glm/trigonometric.hpp>
-// #include <glm/gtx/fast_trigonometry.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/gtx/fast_trigonometry.hpp>
 
-// namespace our
-// {
+namespace our
+{
 
-// 	// The Game Manager system is responsible for controling the game logic.
-// 	// keep track of the score, time etc... 
-// 	class GameMananger {
+	// The Game Manager system is responsible for controling the game logic.
+	// keep track of the score, time etc... 
+	class GameMananger {
 
-// 		Application* app; // The application in which the state runs
-// 		enum states { MENU, PLAY, GAMEOVER };
-// 		states currentState;
-// 		State *State;
-// 	public:
-// 		states menu_state = MENU;
-// 		states play_state = PLAY;
-// 		states gameover_state = GAMEOVER;
-// 		//game score
-// 		static int score;
-// 		static double timePlayed;
-// 		static bool gameOver;
-// 		static bool win;
+		Application* app; // The application in which the state runs
+		enum states { MENU, PLAY, GAMEOVER };
+		states currentState;
+		State *State;
+	public:
+		states menu_state = MENU;
+		states play_state = PLAY;
+		states gameover_state = GAMEOVER;
+		//game score
+		static int score;
+		static double timePlayed;
+		static bool gameOver;
+		static bool win;
 
-// 		//this static object can be used by different logic components 
-// 		static GameMananger gm;
+		//this static object can be used by different logic components 
+		static GameMananger gm;
 
-// 		// When a state enters, it should call this function and give it the pointer to the application
-// 		void enter(Application* app, states state) {
-// 			this->app = app;
-// 			currentState = state;
-// 		}
-// 		//used to add/subtract score ex:coin collection or hit obstacle
-// 		void upScore(int value)
-// 		{
-// 			score += value;
-// 		}
-//         	void downScore(int value)
-// 		{
-// 			score -= value;
-// 		}
-// 		void startGame()
-// 		{
-// 			if(currentState != our::GameMananger::PLAY)
-// 			app->changeState("main");
-// 		}
-// 		// This should be called every frame to update all entities containing a MovementComponent. 
-// 		void update(double deltatime) {
+		// When a state enters, it should call this function and give it the pointer to the application
+		void enter(Application* app, states state) {
+			this->app = app;
+			currentState = state;
+		}
+		//used to add/subtract score ex:coin collection or hit obstacle
+		void upScore(int value)
+		{
+			score += value;
+		}
+        	void downScore(int value)
+		{
+			score -= value;
+		}
+		void startGame()
+		{
+			if(currentState != our::GameMananger::PLAY)
+			app->changeState("main");
+		}
+		// This should be called every frame to update all entities containing a MovementComponent. 
+		void update(double deltatime) {
 		
-// 			if (app->getKeyboard().justPressed(GLFW_KEY_ESCAPE))
-// 			{
-// 				switch (currentState)
-// 				{
-// 				case our::GameMananger::MENU:
-// 					app->changeState("main");
-// 					break;
-// 				case our::GameMananger::PLAY:
-// 					app->changeState("menu");
-// 					break;
-// 				default:
-// 					break;
-// 				}
+			if (app->getKeyboard().justPressed(GLFW_KEY_ESCAPE))
+			{
+				switch (currentState)
+				{
+				case our::GameMananger::MENU:
+					app->changeState("main");
+					break;
+				case our::GameMananger::PLAY:
+					app->changeState("menu");
+					break;
+				default:
+					break;
+				}
                 
-// 			}
-// 		}
-// 	};
-// 	int GameMananger::score = 0;
-// 	double GameMananger::timePlayed = 0.0f;
-// 	bool GameMananger::gameOver = false;
-// 	bool GameMananger::win = false;
+			}
+		}
+	};
+	int GameMananger::score = 0;
+	double GameMananger::timePlayed = 0.0f;
+	bool GameMananger::gameOver = false;
+	bool GameMananger::win = false;
 
-// 	GameMananger GameMananger::gm;
+	GameMananger GameMananger::gm;
 
-// }
+}
